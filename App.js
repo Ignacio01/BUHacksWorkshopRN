0ccaa5342de184a259b9a01c3caf7ed8434f6357
Vardaan Aashish <vardaan@bu.edu>
2017-10-27 18:21:02 -0400

    skeleton

--- App.js
+++ App.js
@@ -14,53 +14,15 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import QuestionPages from './src/QuestionPages.js';
-import ResultPage from './src/ResultPage.js'
-
 class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.welcomeText}>Welcome to the OpenWeb Quiz!</Text>
-        </View>
-        <View style={styles.buttonStyle}>
-          <Button title="Go to the Quiz!" onPress={() => this.props.navigation.navigate('Question')}/>
-        </View>
+      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <Text> Welcome Page </Text>
       </View>
     );
   }
 }
 
-export const RootNavigator = StackNavigator({
-  Home: {
-    screen: App,
-  },
-  Question: {
-    screen: QuestionPages,
-  },
-  Result: {
-    screen: ResultPage
-  }
-});
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcomeText: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  buttonStyle: {
-    marginTop: 10,
-
-  },
-});
-
-export default RootNavigator
+export default App
--- src/ResultPage.js
+++ src/ResultPage.js
@@ -1,85 +1,15 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, Navigator, Image, Button, ListView } from 'react-native';
 
-
 class ResultPage extends Component {
-
-
-  /**
-   * First step: Design
-   *   - Create a View container
-   *   - Create the title view and then the answer view
-   *
-   * Second step: Answers algorithm
-   *   - Create a answerAlgorithm function
-   *      * An function comparing for each question, if the user answer and the correct answer are equivalent
-   *   - Display the result of this function
-   *
-   */
-
 
   render() {
     return (
-      <View style={styles.containerFlex}>
-        <View style={styles.titleView}>
-          <Text style={styles.titleText}>
-            {'Results'}
-          </Text>
-        </View>
-        <View style={styles.answerContainer}>
-          <Text style={styles.answerText}>
-            {'Well done! \n You got ' + this.answerAlgorithm() + ' right answers out of ' + this.props.navigation.state.params.answers.length + ' questions!'}
-          </Text>
-        </View>
+      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <Text> Result Page </Text>
       </View>
     )
   }
-
-  answerAlgorithm(){
-    //Creating a var counting the number of good answers
-    var goodAnswers = 0
-    //Iterating through every answer of the answer array
-    for (var i = 0; i < this.props.navigation.state.params.answers.length; i++){
-      //Checking if the user answer and the correct answer are equivalent
-      if (this.props.navigation.state.params.answers[i] == this.props.navigation.state.params.correctAnswers[i]){
-        //If so, we increment goodAnswers
-        goodAnswers++
-      }
-    }
-    //We then return goodAnswers
-    return goodAnswers
-  }
 }
-
-
-const styles = StyleSheet.create({
-  containerFlex: {
-    flex: 10,
-    borderWidth: 2,
-    borderColor: "#dddddd"
-  },
-  titleView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 2,
-    borderColor: '#dddddd'
-  },
-  answerContainer: {
-    flex: 9,
-    borderWidth: 2,
-    borderColor: '#dddddd',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  titleText: {
-    fontSize: 22
-  },
-  answerText: {
-    fontSize: 20,
-    color: '#000000',
-    textAlign: 'center'
-  }
-});
 
 export default ResultPage
